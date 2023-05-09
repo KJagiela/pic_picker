45f772ec8b9274a7070b9745b5589bf5a8ed3b3b
Kasia <katarzyna.jagiela@tylko.com>
2023-05-09 18:46:10 +0200

    Add models to admin, add path to images

--- backend/apps/challenges/admin.py
+++ backend/apps/challenges/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
+
 from . import models
 
-admin.site.register(models.Challenge)
+
+class ChallengeAdmin(admin.ModelAdmin):
+    readonly_fields = ('owner',)
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        obj.save()
+
+
+class ChallengeEntryAdmin(admin.ModelAdmin):
+    readonly_fields = ('owner',)
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        obj.save()
+
+
+admin.site.register(models.Challenge, ChallengeAdmin)
 admin.site.register(models.PhotoSubject)
-admin.site.register(models.ChallengeEntry)
+admin.site.register(models.ChallengeEntry, ChallengeEntryAdmin)
--- backend/apps/challenges/models.py
+++ backend/apps/challenges/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+def entry_path(instance: 'ChallengeEntry', filename: str) -> str:
+    return f'entry/challenge_{instance.challenge.id}/{filename}'
+
+
 class Challenge(models.Model):
     name = models.CharField(max_length=256)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -19,7 +23,7 @@ class PhotoSubject(models.Model):
 
 
 class ChallengeEntry(models.Model):
-    photo = models.ImageField()
+    photo = models.ImageField(upload_to=entry_path)
     subject = models.ForeignKey('challenges.PhotoSubject', on_delete=models.CASCADE)
     challenge = models.ForeignKey('challenges.Challenge', on_delete=models.CASCADE)
     owner = models.ForeignKey('users.User', on_delete=models.PROTECT)
